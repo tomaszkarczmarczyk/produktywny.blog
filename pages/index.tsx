ff48981a74593970d6e8f124cb6f458339191bd5
Tomasz Karczmarczyk <tokarczmarczyk@gmail.com>
2022-11-06 13:00:40 +0100

    Update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import type { NextPage } from 'next'
+import Layout from '@/layouts/HeaderFooter'
+import getSiteMeta from '@/lib/getSiteMeta'
+import type { NextPageWithLayout } from '@/pages/_app.types'
 import Head from 'next/head'
 
-const Home: NextPage = () => (
-  <div className="container">
-    <Head>
-      <title>produktywny.blog</title>
-    </Head>
-  </div>
-)
+const Home: NextPageWithLayout = () => {
+  const { title, delimiter } = getSiteMeta
+
+  return (
+    <>
+      <Head>
+        <title>{`Strona główna${delimiter}${title}`}</title>
+        <meta name="description" content="Opis strony głównej" key="description" />
+        <meta property="og:description" content="Opis strony głównej" key="ogDescription" />
+      </Head>
+      <main>HOME PAGE</main>
+    </>
+  )
+}
+
+Home.getLayout = (page) => <Layout>{page}</Layout>
 
 export default Home
