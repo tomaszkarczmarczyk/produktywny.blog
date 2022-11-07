03bd9dafee92d8ab46373645657df6fa0900ed5e
Tomasz Karczmarczyk <tokarczmarczyk@gmail.com>
2022-11-07 17:46:51 +0100

    Update blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,17 +1,24 @@
 import ArticleCard from '@/components/ArticleCard'
-import Layout from '@/layouts/HeaderFooter'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import PageHeader from '@/components/PageHeader'
+import Wrapper from '@/components/Wrapper'
 import getFrontmatter from '@/lib/getFrontmatter'
 import getSiteMeta from '@/lib/getSiteMeta'
 import type { ArchivePageProps } from '@/pages/blog/index.types'
 import type { NextPageWithLayout } from '@/pages/_app.types'
 import { readdirSync } from 'fs'
 import type { GetStaticProps } from 'next'
+import Head from 'next/head'
 import { join } from 'path'
 
+const {
+  title,
+  delimiter,
+  paths: { articles: articlesPath },
+} = getSiteMeta
+
 export const getStaticProps: GetStaticProps = async () => {
-  const {
-    paths: { articles: articlesPath },
-  } = getSiteMeta
   const files = readdirSync(join(process.cwd(), ...articlesPath)).filter((file) => file.endsWith('.mdx'))
   const frontmatters = files.map((file) => getFrontmatter(articlesPath, file))
   const articles = frontmatters.filter(({ isPublished }) => isPublished)
@@ -24,15 +31,40 @@ export const getStaticProps: GetStaticProps = async () => {
 }
 
 const ArchivePage: NextPageWithLayout<ArchivePageProps> = ({ articles }) => {
+  const pageTitle = 'Blog'
+  const pageDescription =
+    'Aliqua commodo cillum sit dolor pariatur esse laborum. Commodo dolore sit nulla quis eu ipsum fugiat laborum qui commodo aliquip.'
+  const seoDescription = pageDescription
+
   return (
-    <main>
-      {articles.map((article) => (
-        <ArticleCard {...article} key={article.pathname} />
-      ))}
-    </main>
+    <>
+      <Head>
+        <title>{`${pageTitle}${delimiter}${title}`}</title>
+
+        <meta name="description" content={seoDescription} key="description" />
+
+        <meta property="og:title" content={pageTitle} key="ogTitle" />
+        <meta property="og:description" content={seoDescription} key="ogDescription" />
+        <meta property="og:image" content="" key="ogImage" />
+        <meta property="og:image:width" content="1200" key="ogImageWidth" />
+        <meta property="og:image:height" content="630" key="ogImageHeight" />
+        <meta property="og:image:alt" content="" key="ogImageAlt" />
+      </Head>
+      <Wrapper>
+        <Header />
+        <div>
+          <PageHeader title={pageTitle} description={pageDescription} />
+
+          <main className="grid grid-cols-[1fr_640px_1fr] content-start gap-10 divide-y divide-slate-200 py-10 [&>*+*]:pt-[39px] [&>*+*_a]:before:top-[39px]">
+            {articles.map((article) => (
+              <ArticleCard {...article} key={article.pathname} />
+            ))}
+          </main>
+        </div>
+        <Footer />
+      </Wrapper>
+    </>
   )
 }
-
-ArchivePage.getLayout = (page) => <Layout>{page}</Layout>
 
 export default ArchivePage
