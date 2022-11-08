594365260d568074c70c2642fd1644525fab0085
Tomasz Karczmarczyk <tokarczmarczyk@gmail.com>
2022-11-08 21:20:31 +0100

    Add social media links to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import type { NextComponentType } from 'next'
 import Link from 'next/link'
 import FancyLink from '@/components/FancyLink'
+import SocialMedia from '@/components/SocialMedia'
 
 const Footer: NextComponentType = () => {
   const currentYear: number = new Date().getFullYear()
@@ -9,12 +10,33 @@ const Footer: NextComponentType = () => {
     <footer className="grid grid-cols-[1fr_640px_1fr] bg-slate-200 bg-gradient-to-b from-slate-200 to-slate-400">
       <div className="col-start-2 col-end-3 flex items-center justify-between">
         <span className="text-xs font-bold">© {currentYear} produktywny.blog</span>
-        <FancyLink
-          href="/polityka-prywatnosci"
-          className="text-xs font-bold text-primary focus-visible:rounded focus-visible:outline-none focus-visible:outline-offset-8 focus-visible:outline-rose-400"
-        >
-          Polityka prywatności
-        </FancyLink>
+        <div className="flex items-center space-x-5">
+          <div>
+            <FancyLink
+              href="/polityka-prywatnosci"
+              className="inline-block text-xs font-bold leading-none text-primary focus-visible:rounded focus-visible:outline-none focus-visible:outline-offset-2 focus-visible:outline-rose-400"
+            >
+              Polityka prywatności
+            </FancyLink>
+            {' | '}
+            <FancyLink
+              href="/o-blogu"
+              className="inline-block text-xs font-bold leading-none text-primary focus-visible:rounded focus-visible:outline-none focus-visible:outline-offset-2 focus-visible:outline-rose-400"
+            >
+              O blogu
+            </FancyLink>
+            {' | '}
+            <FancyLink
+              href="mailto:tomasz@produktywny.blog"
+              className="inline-block text-xs font-bold leading-none text-primary focus-visible:rounded focus-visible:outline-none focus-visible:outline-offset-2 focus-visible:outline-rose-400"
+            >
+              Kontakt
+            </FancyLink>
+          </div>
+          <div className="flex items-center space-x-3">
+            <SocialMedia />
+          </div>
+        </div>
       </div>
     </footer>
   )
