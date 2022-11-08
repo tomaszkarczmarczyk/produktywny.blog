aa84de6098007a70018936bf6d1756cbd01cf042
Tomasz Karczmarczyk <tokarczmarczyk@gmail.com>
2022-11-08 21:16:46 +0100

    Add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import GlobalContextProvider from '@/context/GlobalContext'
 import Layout from '@/layouts/Default'
 import type { AppPropsWithLayout } from '@/pages/_app.types'
 import '@/styles/globals.css'
+import NextNProgress from 'nextjs-progressbar'
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page)
@@ -9,6 +10,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   return getLayout(
     <GlobalContextProvider>
       <Layout>
+        <NextNProgress color="#fb7185" startPosition={0} height={4} options={{ showSpinner: false }} />
         <Component {...pageProps} />
       </Layout>
     </GlobalContextProvider>
